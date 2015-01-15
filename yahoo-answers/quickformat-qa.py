aa33f3bc6e4b9fcbade72be198d8fd261a34e9e6
beeva-nievesabalos <nieves.abalos@beeva.com>
2015-01-15 18:42:23 +0100

    arreglado el formateo de XML enormes en el script de python

--- yahoo-answers/quickformat-qa.py
+++ yahoo-answers/quickformat-qa.py
@@ -6,7 +6,10 @@ Created on Tue Jan 13 18:14:14 2015
 
 @author: nievesabalos
 """
-#import xml.etree.ElementTree as ET
+import sys
+print "Encoding:"
+print sys.getdefaultencoding()
+
 import xml.etree.cElementTree as ET
 import HTMLParser
 import logging, os
@@ -24,7 +27,10 @@ def FormatData(line):
     #print repeticiones
     if repeticiones != 0:
         fraseLimpia = fraseLimpia.replace(saltoDeCarro, "", repeticiones)
-    return fraseLimpia
+    #print fraseLimpia        
+    #lineTemp = fraseLimpia.decode('ascii')
+    newLine = fraseLimpia.encode('utf-8') #, 'ignore'
+    return newLine
 
 pathData = '../data'
 pathSmallData = '../datasmall'
@@ -32,30 +38,39 @@ fileToRead = '../data/FullOct2007.xml'
 smallFileToRead = '../datasmall/small_sample.xml'
 
 print "+ Fichero que voy a leer:"
-print smallFileToRead
+print fileToRead
 print "\nGenero un árbol para el contenido del archivo XML..."
 
 def procesarElemento(nodoRoot):
-   print "\nRecorro un elemento '<vespaadd>'..."
+   #print "\nRecorro un elemento '<vespaadd>'..."
    for nodo in nodoRoot.findall("./document"):
-       idioma = nodo.find('language').text.encode('utf8')
-       print idioma
-       if idioma == 'es-es':#'en-us':
+       idioma = nodo.find('qlang').text
+       if idioma != 'en':
+           print idioma
+       if idioma == 'es':#'en-us':
+           language = nodo.find('language').text
            print "\nExtraigo del elemento..." 
-           categoria = nodo.find('cat').text.encode('utf8')
-           categoriaPrincipal = nodo.find('maincat').text.encode('utf8')
-           subcategoria = nodo.find('subcat').text.encode('utf8')
-           pregunta = FormatData(nodo.find('subject').text).encode('utf8')
-           contenido = FormatData(nodo.find('content').text).encode('utf8')
-           mejorRespuesta = FormatData(nodo.find('bestanswer').text).encode('utf8')
-           otrasRespuestas = []
-           for respuestas in nodo.findall('nbestanswers/answer_item'):
-               otra = respuestas.text.encode('utf8')
-               otrasRespuestas.append(FormatData(otra))
+           categoria = FormatData(nodo.find('cat').text)
+           categoriaPrincipal = FormatData(nodo.find('maincat').text)
+           subcategoria = FormatData(nodo.find('subcat').text)
+           pregunta = FormatData(nodo.find('subject').text)
+           contenido = None
+           if nodo.find('content'):
+               contenido = FormatData(nodo.find('content').text)
+           mejorRespuesta = None    
+           if nodo.find('bestanswer'):    
+               mejorRespuesta = FormatData(nodo.find('bestanswer').text)
+           otrasRespuestas = None
+           if nodo.find('nbestanswers'):
+               otrasRespuestas = []
+               for respuestas in nodo.findall('nbestanswers/answer_item'):
+                   otrasRespuestas.append(FormatData(respuestas.text))
                
            with open('dataYahooQA.txt', 'a') as fileData:
              print "Escribo en fichero pregunta y respuestas sobre:"
              print pregunta
+             fileData.write('\nIDIOMA\n')
+             fileData.write(language)
              fileData.write('\nCATEGORÍA\n')
              fileData.write(categoria)
              fileData.write('\nCATEGORÍA PRINCIPAL\n')
@@ -64,14 +79,17 @@ def procesarElemento(nodoRoot):
              fileData.write(subcategoria)
              fileData.write('\nPREGUNTA\n')
              fileData.write(pregunta)
-             fileData.write('\nCONTENIDO\n')
-             fileData.write(contenido)
-             fileData.write('\nMEJOR RESPUESTA\n')
-             fileData.write(mejorRespuesta)
-             fileData.write('\nOTRAS RESPUESTAS\n')
-             for respuestas in otrasRespuestas:
-                 fileData.write(respuestas) 
-                 fileData.write('\n')
+             if contenido:
+                 fileData.write('\nCONTENIDO\n')
+                 fileData.write(contenido)
+             if mejorRespuesta:
+                 fileData.write('\nMEJOR RESPUESTA\n')
+                 fileData.write(mejorRespuesta)
+             if otrasRespuestas: 
+                 fileData.write('\nOTRAS RESPUESTAS\n')
+                 for respuestas in otrasRespuestas:
+                     fileData.write(respuestas) 
+                     fileData.write('\n')
                  
              print "Cierro fichero."
              fileData.close()
@@ -85,7 +103,7 @@ def process_buffer(buf):
     #pull it apart and stick it in the database
 
 inputbuffer = ''
-with open(smallFileToRead,'rb') as inputfile:
+with open(fileToRead,'rb') as inputfile:
     append = False
     for line in inputfile:
         if '<vespaadd>' in line:
