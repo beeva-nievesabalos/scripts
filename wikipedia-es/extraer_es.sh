a17444f3dc910d403dba9e2451bf8f5e99119354
Nieves Abalos <nieves.abalos@beeva.com>
2015-01-21 16:26:12 +0100

    Update extraer_es.sh

--- wikipedia-es/extraer_es.sh
+++ wikipedia-es/extraer_es.sh
@@ -1,6 +1,6 @@
 wget http://dumps.wikimedia.org/eswiki/20150105/eswiki-20150105-pages-articles.xml.bz2
 
-bzcat eswiki-20150105-pages-articles.xml.bz2 |  WikiExtractor.py -cb 500K -o extracted
+bzcat eswiki-20150105-pages-articles.xml.bz2 |  ./extractor/WikiExtractor.py -cb 500K -o extracted
 
 find extracted -name '*bz2' -exec bunzip2 -c {} \; > wikipedia_es.xml
 
