7dc33d501a4cd1f49caf8723f75db8f7b5074ce7
beeva-nievesabalos <nieves.abalos@beeva.com>
2015-01-21 16:09:18 +0100

    added wikipedia scripts

---
+++ wikipedia-es/extraer_es.sh
@@ -0,0 +1,7 @@
+wget http://dumps.wikimedia.org/eswiki/20150105/eswiki-20150105-pages-articles.xml.bz2
+
+bzcat eswiki-20150105-pages-articles.xml.bz2 |  WikiExtractor.py -cb 500K -o extracted
+
+find extracted -name '*bz2' -exec bunzip2 -c {} \; > wikipedia_es.xml
+
+rm -rf extracted
