193abdc409a3d4f9cbf3b9519ec272ba07307566
beeva-nievesabalos <nieves.abalos@beeva.com>
2015-01-22 10:54:41 +0100

    scripts wiki small

--- wikipedia-es/extraer_es.sh
+++ wikipedia-es/extraer_es.sh
@@ -1,6 +1,8 @@
-wget http://dumps.wikimedia.org/eswiki/20150105/eswiki-20150105-pages-articles.xml.bz2
+if [ ! -e eswiki-20150105-pages-articles.xml.bz2 ]; then
+    wget http://dumps.wikimedia.org/eswiki/20150105/eswiki-20150105-pages-articles.xml.bz2
+fi
 
-bzcat eswiki-20150105-pages-articles.xml.bz2 |  ./extractor/WikiExtractor.py -cb 500K -o extracted
+bzcat eswiki-20150105-pages-articles.xml.bz2 | ./WikiExtractor.py -cb 500K -o extracted
 
 find extracted -name '*bz2' -exec bunzip2 -c {} \; > wikipedia_es.xml
 
---
+++ wikipedia-es/extraer_es_small.sh
@@ -0,0 +1,9 @@
+f [ ! -e eswiki-20150105-pages-articles2.xml.bz2 ]; then
+    wget https://dumps.wikimedia.org/eswiki/20150105/eswiki-20150105-pages-articles2.xml.bz2 
+fi
+
+bzcat eswiki-20150105-pages-articles2.xml.bz2 | ./WikiExtractor.py -cb 500K -o extracted
+
+find extracted -name '*bz2' -exec bunzip2 -c {} \; > wikipedia_es_small.xml
+
+rm -rf extracted
